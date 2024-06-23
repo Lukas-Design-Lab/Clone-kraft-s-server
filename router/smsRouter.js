afbfc2077da0e2e17224c765bde16ee8c716b750
Ibeneme <macbook@Ibeneme-Ikennas.local>
2024-06-23 08:28:09 +0100

    updates-soft

--- router/smsRouter.js
+++ router/smsRouter.js
@@ -11,7 +11,7 @@ const sendSMS = async (to, message) => {
       type: "plain", // or 'flash' depending on your needs
       channel: "generic",
       api_key: TERMII_API_KEY,
-      from: "ChatWazobia",
+      from: "Clonekraft Team",
     });
     console.log("SMS sent successfully:", response.data);
   } catch (error) {
